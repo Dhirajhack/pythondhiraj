c6b5f8951dc8be2184e47d18fcba9e4325198d16
Dhirajhack <122712894+Dhirajhack@users.noreply.github.com>
2026-03-09 10:14:27 +0000

    dhiraj

--- patten1.py
+++ patten1.py
@@ -1,8 +1,58 @@
-# for i in range(3):
-#     print("*")
+# # for i in range(3):
+# #     print("*")
 
-for i in range(3):
-    for j in range(3):
-        print("*" , end="")
-    print()    
+# for i in range(3):
+#     for j in range(3):
+#         print("*" , end="")
+#     print()    
     
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i , end="")
+#     print()    
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i , end="")
+#     print()    
+
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(j, end="")
+#     print()    
+            
+# row=int(input("enter number"))
+# col=int(input("enter coloum count"))
+
+# for i in range(row):
+#     for j in range(col):
+#         print(j, end="")
+#     print()
+
+# for i in range(5):
+#     for j in range(5):
+#         if i==j:
+#             print(1 , end="")
+#         else :
+#             print(0 , end="")    
+       
+#     print()
+
+#left increment  traingle
+
+
+count=int(input("enter number"))
+n=1
+for i in range(count):
+    for j in range(i):
+        print(n , end="")
+        n+=1
+    print()   
+
+
+
