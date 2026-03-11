14f8f3fe87f50f82d95fc95810f5848d5db1afda
Dhirajhack <122712894+Dhirajhack@users.noreply.github.com>
2026-03-11 10:20:26 +0000

    dhiraj

--- libarycurd.py
+++ libarycurd.py
@@ -4,7 +4,7 @@ while True:
     print("1.addbooks\n2:ShowBooks\n3:UpdateBook\n4:DeleteBook\n5:Exit")
     choice=int(input("enter Choice number: "))
     if choice==1:
-        title=input("Enter Book title").lower()
+        title=input("Enter Book title").capitalize()
         if title not in books:
 
             books.append(title)
@@ -17,9 +17,28 @@ while True:
         else:
             print("avaliable books are" , books)    
     elif choice==3:
-        pass
+        if len(books)>0:
+            old_book_title=input("Enter book title to update").capitalize()
+            if old_book_title in books:
+                new_title=input("Enter new title od book to update.").capitalize()
+                books[books.index(old_book_title)]=new_title.capitalize()
+                print(f"{old_book_title}book title updated succesfully")
+            else:
+                print(f"{old_book_title} book tile is not found try again")    
+        else:
+            print("no book available so first addd to book !!")        
     elif choice==4:
-        pass
+        if len(books)>0:
+            old_book_title=input("Enter book title to remove").capitalize()
+            if old_book_title in books:
+                
+                books.remove(old_book_title.capitalize())
+                print(f"{old_book_title}Books removed succesfully")
+            else:
+                print(f"{old_book_title} book tile is not found try again")    
+        else:
+            print("no book available so first addd to book !!")   
+    
     elif choice==5:
         print("Thanks for using our servies !!")
         break
---
+++ listmethod.py
@@ -0,0 +1,14 @@
+fruits=["apple" , "grapes" ,"kiwi" , "banana" , "watermelon"]
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.sort()
+print(fruits)
+
+price=[100,300,200,400,600]
+fruits.extend(price)
+print(fruits)
+
+
